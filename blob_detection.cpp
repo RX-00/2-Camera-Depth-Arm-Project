7a6a5f97730a97b71abb01f9e8da4c185216a89f
RX-00 <00royxing@gmail.com>
2016-06-01 19:16:51 -0400

    Add files via upload

--- blob_detection.cpp
+++ blob_detection.cpp
@@ -1,6 +1,6 @@
-//TODO PERHAPS YOU NEED TWO TRACKBARS FOR EACH COLOR FOR EACH CAMERA, THUS YOU HAVE 8 WINDOWS TOTAL, 2 FOR GREEN, 2 FOR RED ALL FOR TOP CAMERA, 2 FOR GREEN, 2 FOR RED ALL FOR SIDE CAMERA
-//TODO SEE IF YOU CAN SENSE RED AT THE SAME TIME, PROBABLY GOING TO NEED A SEPARATE WINDOW AND TRACKBAR OR SOMETHING
 //TODO INTEGRATE SENDING INFO OVER SERIAL TO ARDUINO
+//TODO PERHAPS CHANGE THE INT MAIN INTO ITS OWN FUNCTION AND HAVE IT RETURN OR MODIFY THE iLastX AND iLastY VALUES TO BE SENT BY A SERIAL SENDING PROGRAM
+//TODO FOR THE SERIAL INFO SEE IF YOU CAN USE ONE OF THE NUMBERS SENT TO CONTORL WHICH SERVO IS GOING TO BE USED
 
 #include <iostream>
 #include <sstream>
@@ -12,10 +12,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
 
+//Position to send out to arduino to move servos
+int posBase = 0;
+
+//Distance from Red LED to Green Ball Top, and Side
+int DistanceTop = 0;
+int DistanceSide = 0;
+
+//Variables for Red Blob Detection Trackbar
+int LowH = 0;
+int HighH = 179;
+
+int LowS = 18;
+int HighS = 255;
+
+int LowV = 255;
+int HighV = 255;
+
+//Variables for Green Blob Detection Trackbar
 int iLowH = 46;
 int iHighH = 124;
 
@@ -25,7 +44,43 @@ int iHighS = 255;
 int iLowV = 60;
 int iHighV = 255;
 
-void createTrackbars(){
+//Points for Green Object
+int iLastX = -1;
+int iLastY = -1;
+
+//Points for Red Object
+int LastX = -1;
+int LastY = -1;
+
+//Test when the points are closest
+void pointsClosest(double){
+  //sendInfo();
+  //distanceFormula();
+}
+
+//Calculate the distance between the points
+int distanceFormula(int, int, int, int){
+  int result;
+
+  DistanceTop = sqrt(pow((LastX-iLastX), 2) + pow((LastY-iLastY), 2));
+  return DistanceTop;
+  
+}
+
+void createTrackbarsRed(){
+  namedWindow("Control Red", CV_WINDOW_AUTOSIZE);
+
+  cvCreateTrackbar("LowH", "Control Red", &LowH, 179);
+  cvCreateTrackbar("HighH", "Control Red", &HighH, 179);
+
+  cvCreateTrackbar("LowS", "Control Red", &LowS, 255);
+  cvCreateTrackbar("HighS", "Control Red", &HighS, 255);
+
+  cvCreateTrackbar("LowV", "Control Red", &LowV, 255);
+  cvCreateTrackbar("HighV", "Control Red", &HighV, 255);
+}
+
+void createTrackbarsGreen(){
   namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
   //Create trackbars in "Control" window
@@ -39,20 +94,38 @@ void createTrackbars(){
   cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 }
 
+
+void sendInfo(){
+  int information[]={posBase, posBase};
+  FILE *arduino;
+  arduino = fopen("/dev/ttyACM0", "w");
+  if(arduino == NULL){
+    cout<<"error: cannot connect to device /dev/ttyACM0"<<endl;
+  }
+  int i = 0;
+  for(i = 0; i<2; i++){
+    cout<<"sending info"<<endl;
+    fprintf(arduino, "%d", information[i]);
+    fprintf(arduino, "%c", ',');
+    sleep(1);
+  }
+  cout<<"Information has been sent: "<<posBase<<endl;
+  fclose(arduino);
+}
+
+
 int main( int argc, char** argv ){
 
     VideoCapture cap(1); //capture the video from webcam
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
+    // if not success, exit program
+    if ( !cap.isOpened() ){
          cout << "Cannot open the web cam" << endl;
          return -1;
     }
 
-    createTrackbars();
-
-    int iLastX = -1;
-    int iLastY = -1;
+    createTrackbarsGreen();
+    createTrackbarsRed();
 
     //Capture a temporary image from the camera
     Mat imgTmp;
@@ -60,6 +133,7 @@ int main( int argc, char** argv ){
 
     //Create a black image with the size as the camera output
     Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
+    Mat imgLinesRed = Mat::zeros(imgTmp.size(), CV_8UC3);;
 
 
     while (true){
@@ -76,49 +150,79 @@ int main( int argc, char** argv ){
       cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
       Mat imgThresholded;
+      Mat imgThresholdedRed;
 
-      inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image for green
+
+      inRange(imgHSV, Scalar(LowH, LowS, LowV), Scalar(HighH, HighS, HighV), imgThresholdedRed); //Threshold the image for red
 
       //morphological opening (removes small objects from the foreground)
       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-      dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+      dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+      erode(imgThresholdedRed, imgThresholdedRed, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+      dilate(imgThresholdedRed, imgThresholdedRed, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
       //morphological closing (removes small holes from the foreground)
       dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
+      dilate( imgThresholdedRed, imgThresholdedRed, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+      erode(imgThresholdedRed, imgThresholdedRed, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
       //Calculate the moments of the thresholded image
       Moments oMoments = moments(imgThresholded);
+      Moments oMomentsred = moments(imgThresholdedRed);
 
       double dM01 = oMoments.m01;
       double dM10 = oMoments.m10;
       double dArea = oMoments.m00;
 
+      double dM01Red = oMomentsred.m01;
+      double dM10Red = oMomentsred.m10;
+      double dAreaRed = oMomentsred.m00;
+
       // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zeros
-      if (dArea > 10000){
+      if (dArea > 9000 || dAreaRed > 500){
+
+        //calculate the position of the Red LED
+        int posXred = dM10Red / dAreaRed;
+        int posYred = dM01Red / dAreaRed;
 
         //calculate the position of the ball
         int posX = dM10 / dArea;
         int posY = dM01 / dArea;
-        if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0){
+
+        if ((iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) && (LastX >= 0 && LastY >= 0 && posXred >= 0 && posYred >= 0)){
           //Draw a red line from the previous point to the current point
           line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+
+          line(imgLinesRed, Point(posXred, posYred), Point(LastX, LastY), Scalar(255,0,0), 2);
         }
 
         iLastX = posX;
         iLastY = posY;
+
+        LastX = posXred;
+        LastY = posYred;
       }
 
       imshow("Thresholded Image", imgThresholded); //show the thresholded image
+      imshow("Thresholded Image Red", imgThresholdedRed);
 
-      imgOriginal = imgOriginal + imgLines;
+      imgOriginal = imgOriginal + imgLines + imgLinesRed;
       imshow("Original", imgOriginal); //show the original image
-      cout<<"X position = "<<iLastX<<" Y position = "<<iLastY<<endl;
+      cout<<"X green position = "<<iLastX<<" Y green position = "<<iLastY<<endl;
+      cout<<"X red position = "<<LastX<<" Y red position ="<<LastY<<endl;
+
+      distanceFormula(iLastX, iLastY, LastX, LastY);
+      cout<<DistanceTop<<endl;
 
       if (waitKey(30) == 27){ //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
         cout << "esc key is pressed by user" << endl;
         break;
        }
     }
+
     return 0;
 }
